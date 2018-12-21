ba0905b2468dca2888ce64c7f8af2cc80ca83243
Ashish <ashish.shete85@gmail.com>
2018-12-21 22:29:03 +0530

    first commit

---
+++ TwinImageCrop/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TwinImageCrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRefLine = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cbxLockBoth = new System.Windows.Forms.CheckBox();
+            this.leftZoomBar = new System.Windows.Forms.TrackBar();
+            this.rightZoomTrack = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftZoomBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightZoomTrack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(118, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnRefLine
+            // 
+            this.btnRefLine.Location = new System.Drawing.Point(12, 52);
+            this.btnRefLine.Name = "btnRefLine";
+            this.btnRefLine.Size = new System.Drawing.Size(75, 23);
+            this.btnRefLine.TabIndex = 1;
+            this.btnRefLine.Text = "Ref Line";
+            this.btnRefLine.UseVisualStyleBackColor = true;
+            this.btnRefLine.Click += new System.EventHandler(this.btnRefLine_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(731, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // cbxLockBoth
+            // 
+            this.cbxLockBoth.AutoSize = true;
+            this.cbxLockBoth.Location = new System.Drawing.Point(12, 82);
+            this.cbxLockBoth.Name = "cbxLockBoth";
+            this.cbxLockBoth.Size = new System.Drawing.Size(75, 17);
+            this.cbxLockBoth.TabIndex = 3;
+            this.cbxLockBoth.Text = "Lock Both";
+            this.cbxLockBoth.UseVisualStyleBackColor = true;
+            this.cbxLockBoth.CheckedChanged += new System.EventHandler(this.cbxLockBoth_CheckedChanged);
+            // 
+            // leftZoomBar
+            // 
+            this.leftZoomBar.Location = new System.Drawing.Point(42, 161);
+            this.leftZoomBar.Name = "leftZoomBar";
+            this.leftZoomBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.leftZoomBar.Size = new System.Drawing.Size(45, 192);
+            this.leftZoomBar.TabIndex = 4;
+            this.leftZoomBar.Scroll += new System.EventHandler(this.leftZoomBar_Scroll);
+            // 
+            // rightZoomTrack
+            // 
+            this.rightZoomTrack.Location = new System.Drawing.Point(669, 161);
+            this.rightZoomTrack.Name = "rightZoomTrack";
+            this.rightZoomTrack.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.rightZoomTrack.Size = new System.Drawing.Size(45, 192);
+            this.rightZoomTrack.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 538);
+            this.Controls.Add(this.rightZoomTrack);
+            this.Controls.Add(this.leftZoomBar);
+            this.Controls.Add(this.cbxLockBoth);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnRefLine);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftZoomBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightZoomTrack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRefLine;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox cbxLockBoth;
+        private System.Windows.Forms.TrackBar leftZoomBar;
+        private System.Windows.Forms.TrackBar rightZoomTrack;
+    }
+}
+
---
+++ TwinImageCrop/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace TwinImageCrop
+{
+    public partial class Form1 : Form
+    {
+        // https://stackoverflow.com/questions/12054918/how-to-pan-image-inside-picturebox
+
+        private Point m_startingPoint = Point.Empty;
+        private Point m_movingPoint = Point.Empty;
+        private Point m_drawStartingPt = Point.Empty;
+        private bool m_bPanning = false;
+        private bool m_bRefLineDrawingOn = false;
+        private Dictionary<string, MyPoint> stringPtMap = new Dictionary<string, MyPoint>();
+        private Image m_leftOriginalImage;
+
+        const string PAN_FIRST_PT = "PAN_FIRST_PT";
+        const string PAN_MOVING_PT = "PAN_MOVING_PT";
+
+        public Form1()
+        {
+            InitializeComponent();
+            stringPtMap.Add(pictureBox1.Name + PAN_FIRST_PT, new MyPoint(0, 0));
+            stringPtMap.Add(pictureBox1.Name + PAN_MOVING_PT, new MyPoint(0, 0));
+            stringPtMap.Add(pictureBox2.Name + PAN_FIRST_PT, new MyPoint(0, 0));
+            stringPtMap.Add(pictureBox2.Name + PAN_MOVING_PT, new MyPoint(0, 0));
+
+            m_leftOriginalImage = new Bitmap(pictureBox1.Image);
+        }
+
+        void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            var pictureBoxes = new List<PictureBox>();
+            if (cbxLockBoth.Checked)
+            {
+                pictureBoxes.Add(pictureBox1);
+                pictureBoxes.Add(pictureBox2);
+            }
+            else
+            {
+                pictureBoxes.Add(sender as PictureBox);
+            }
+
+            foreach (var picBox in pictureBoxes)
+            {
+                m_bPanning = true;
+                var panFirstPt = stringPtMap[picBox.Name + PAN_FIRST_PT];
+                var panMovingPt = stringPtMap[picBox.Name + PAN_MOVING_PT];
+
+                panFirstPt.m_Pt.X = e.Location.X - panMovingPt.m_Pt.X;
+                panFirstPt.m_Pt.Y = e.Location.Y - panMovingPt.m_Pt.Y;
+
+                if (m_bRefLineDrawingOn)
+                {
+                    m_leftOriginalImage = new Bitmap(picBox.Image);
+                    m_drawStartingPt = new Point(m_startingPoint.X, m_startingPoint.Y);
+                }
+            }
+            
+        }
+
+        void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bPanning = false;
+        }
+
+        void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            var pictureBoxes = new List<PictureBox>();
+            if (cbxLockBoth.Checked)
+            {
+                pictureBoxes.Add(pictureBox1);
+                pictureBoxes.Add(pictureBox2);
+            }
+            else
+            {
+                pictureBoxes.Add(sender as PictureBox);
+            }
+
+            foreach (var picBox in pictureBoxes)
+            {
+
+                if (m_bPanning)
+                {
+                    var panFirstPt = stringPtMap[picBox.Name + PAN_FIRST_PT];
+                    var panMovingPt = stringPtMap[picBox.Name + PAN_MOVING_PT];
+
+                    panMovingPt.m_Pt.X = e.Location.X - panFirstPt.m_Pt.X;
+                    panMovingPt.m_Pt.Y = e.Location.Y - panFirstPt.m_Pt.Y;
+
+                    picBox.Invalidate();
+                }
+
+                // If we are drawing on the canvas
+                if (m_bRefLineDrawingOn && m_drawStartingPt != Point.Empty)
+                {
+                    DrawLine(picBox, m_drawStartingPt.X, m_drawStartingPt.Y, e.Location.X + m_movingPoint.X, e.Location.Y + m_movingPoint.Y);
+                    picBox.Invalidate();
+                }
+            }
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // This will be called by individual PictureBox, so no need to synchronise here.
+            var picBox = sender as PictureBox;
+            var panMovingPt = stringPtMap[picBox.Name + PAN_MOVING_PT];
+
+            e.Graphics.Clear(Color.White);
+            e.Graphics.DrawImage(picBox.Image, panMovingPt.m_Pt);
+        }
+
+        private void DrawLine(PictureBox picBox, int x1, int y1, int x2, int y2)
+        {
+            picBox.Image.Dispose();
+            
+            Graphics g;
+            pictureBox1.Image = new Bitmap(m_leftOriginalImage);
+            // g = Graphics.FromImage(pictureBox1.Image);
+
+
+            g = Graphics.FromImage(pictureBox1.Image);
+            pictureBox1.Refresh();
+
+            Pen mypen = new Pen(Color.Black);
+            g.DrawLine(mypen, x1, y1, x2, y2);
+            g.Dispose();
+        }
+
+        private void btnRefLine_Click(object sender, EventArgs e)
+        {
+            if(!m_bRefLineDrawingOn)
+            {
+                (sender as Button).BackColor = Color.Red;
+            }
+            else
+            {
+                (sender as Button).BackColor = Button.DefaultBackColor;
+                m_drawStartingPt = Point.Empty;
+            }
+            m_bRefLineDrawingOn = !m_bRefLineDrawingOn;
+        }
+
+        private void cbxLockBoth_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbxLockBoth.Checked)
+            {
+                var leftWindowMovingPt = stringPtMap[pictureBox1.Name + PAN_MOVING_PT];
+                var rightWindowMovingPt = stringPtMap[pictureBox2.Name + PAN_MOVING_PT];
+
+                leftWindowMovingPt.m_Pt = rightWindowMovingPt.m_Pt;
+            }
+        }
+
+
+        public Image PictureBoxZoom(Image img, Size size)
+        {
+            Bitmap bm = new Bitmap(img, Convert.ToInt32(img.Width * size.Width), Convert.ToInt32(img.Height * size.Height));
+            Graphics grap = Graphics.FromImage(bm);
+            grap.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            return bm;
+        }
+
+        private void leftZoomBar_Scroll(object sender, EventArgs e)
+        {
+            if (leftZoomBar.Value > 0)
+            {
+                pictureBox1.Image = null;
+                pictureBox1.Image = PictureBoxZoom(m_leftOriginalImage, new Size(leftZoomBar.Value, leftZoomBar.Value));
+            }
+        }
+    }
+
+    public class MyPoint
+    {
+        public Point m_Pt;
+        public MyPoint(int x, int y)
+        {
+            m_Pt.X = x;
+            m_Pt.Y = y;
+        }
+
+        private MyPoint()
+        {
+
+        }
+    }
+}
